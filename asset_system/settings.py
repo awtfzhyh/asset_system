e46f603824f6f891b25d95ef1e8d67446a5fc1f6
awtfzhyh <awatifzahiyah0509@gmail.com>
2026-06-14 23:29:45 +0800

    Switch SMTP configuration to SSL port 465 for cloud compatibility

--- asset_system/settings.py
+++ asset_system/settings.py
@@ -146,8 +146,9 @@ LOGIN_URL = '/login/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = 'awatifzahiyah0509@gmail.com'
 EMAIL_HOST_PASSWORD = 'xdga xnvk jewp xouz'
 DEFAULT_FROM_EMAIL = 'CoE Asset System <awatifzahiyah0509@gmail.com>'
